38f9db9628287d21a691c192f49dec5a238fa1e1
zerratar <zerratar@gmail.com>
2015-12-26 16:30:08 +0100

    Added new project: PexInspector, a visual tool for browsing and modifying .pex files. Right now, you can only open up pex files, list all content and view method instructions. The aim is to be able to insert/inject, edit and remove instructions from existing scripts. As well for adding new properties, fields, methods in a simple and fast way. This makes it possible for people to change the behaviour of the current scripts... Or what not.. :-)

---
+++ Source/PapyrusDotNet.PexInspector.ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,212 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Windows.Input;
+using GalaSoft.MvvmLight;
+using GalaSoft.MvvmLight.Command;
+using Microsoft.Win32;
+using PapyrusDotNet.PapyrusAssembly;
+
+namespace PapyrusDotNet.PexInspector.ViewModels
+{
+    public class MainWindowViewModel : ViewModelBase
+    {
+        private PapyrusAssemblyDefinition loadedAssembly;
+        private string loadedAssemblyName;
+        public MainWindowViewModel()
+        {
+            ExitCommand = new RelayCommand(Exit);
+            OpenPexCommand = new RelayCommand(OpenPex);
+            SavePexCommand = new RelayCommand(SavePex, CanSave);
+            SavePexAsCommand = new RelayCommand(SavePexAs, CanSave);
+
+            SelectedMemberCommand = new RelayCommand<PapyrusViewModel>(SelectMember);
+
+            InsertAfterCommand = new RelayCommand<PapyrusInstruction>(InsertAfter, CanInsert);
+            InsertBeforeCommand = new RelayCommand<PapyrusInstruction>(InsertBefore, CanInsert);
+            EditInstructionCommand = new RelayCommand<PapyrusInstruction>(EditInstruction, CanInsert);
+            RemoveInstructionCommand = new RelayCommand<PapyrusInstruction>(RemoveInstruction, CanInsert);
+        }
+
+        private void RemoveInstruction(PapyrusInstruction obj)
+        {
+        }
+
+        private void EditInstruction(PapyrusInstruction obj)
+        {
+        }
+
+        private void InsertBefore(PapyrusInstruction obj)
+        {
+        }
+
+        private bool CanInsert(PapyrusInstruction arg) => arg != null;
+
+        private void InsertAfter(PapyrusInstruction obj)
+        {
+
+        }
+
+        private void Exit()
+        {
+
+        }
+
+        private void SavePexAs()
+        {
+
+        }
+
+        private bool CanSave() => loadedAssembly != null;
+
+        private void SavePex()
+        {
+
+        }
+
+        private void OpenPex()
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.Filter = "Papyrus Script Binary (*.pex)|*.pex";
+            if (ofd.ShowDialog().GetValueOrDefault())
+            {
+                LoadPex(ofd.FileName);
+            }
+        }
+
+        private void LoadPex(string fileName)
+        {
+            loadedAssembly = PapyrusAssemblyDefinition.ReadAssembly(fileName);
+            loadedAssemblyName = System.IO.Path.GetFileName(fileName);
+            BuildPexTree();
+
+            SavePexCommand.RaiseCanExecuteChanged();
+            SavePexAsCommand.RaiseCanExecuteChanged();
+        }
+
+        private void BuildPexTree()
+        {
+            var asm = loadedAssembly;
+            var root = new PapyrusViewModel();
+            root.Item = "root";
+            root.Text = loadedAssemblyName;
+            foreach (var type in asm.Types)
+            {
+                var typeNode = new PapyrusViewModel(root);
+                typeNode.Item = type;
+                typeNode.Text = type.Name.Value;
+                foreach (var structType in type.NestedTypes)
+                {
+                    var structTypeNode = new PapyrusViewModel(typeNode);
+                    structTypeNode.Item = structType;
+                    structTypeNode.Text = structType.Name.Value;
+                    foreach (var field in structType.Fields)
+                    {
+                        var fieldNode = new PapyrusViewModel(structTypeNode);
+                        fieldNode.Item = field;
+                        fieldNode.Text = field.Name.Value + " : " + field.TypeName;
+                    }
+                }
+
+                var statesNode = new PapyrusViewModel(typeNode);
+                statesNode.Item = "states";
+                statesNode.Text = "States";
+                foreach (var item in type.States)
+                {
+                    var stateNode = new PapyrusViewModel(statesNode);
+                    stateNode.Item = item;
+                    stateNode.Text = (!string.IsNullOrEmpty(item.Name.Value) ? item.Name.Value : "default");
+                    foreach (var method in item.Methods)
+                    {
+                        var m = new PapyrusViewModel(stateNode);
+                        m.Item = method;
+                        m.Text = method.Name.Value + GetParameterString(method.Parameters) + " : " +
+                                 method.ReturnTypeName.Value;
+                    }
+                }
+
+                foreach (var field in type.Fields)
+                {
+                    var fieldNode = new PapyrusViewModel(typeNode);
+                    fieldNode.Item = field;
+                    fieldNode.Text = field.Name.Value + " : " + field.TypeName;
+                }
+
+                foreach (var item in type.Properties)
+                {
+                    var fieldNode = new PapyrusViewModel(typeNode);
+                    fieldNode.Item = item;
+                    fieldNode.Text = item.Name.Value + " : " + item.TypeName.Value;
+                }
+
+
+            }
+            PexTree = new ObservableCollection<PapyrusViewModel>(new[] { root });
+        }
+
+        private string GetParameterString(List<PapyrusParameterDefinition> parameters, bool includeParameterNames = false)
+        {
+            var paramDefs = string.Join(", ", parameters.Select(p => p.TypeName.Value +
+            (includeParameterNames ? (" " + p.Name.Value) : "")));
+
+            return "(" + paramDefs + ")";
+        }
+
+        public ObservableCollection<PapyrusViewModel> PexTree
+        {
+            get { return pexTree; }
+            set { Set(ref pexTree, value); }
+        }
+
+        private void SelectMember(PapyrusViewModel item)
+        {
+            var method = item.Item as PapyrusMethodDefinition;
+
+            if (method != null)
+            {
+                SelectedMethodInstructions = new ObservableCollection<PapyrusInstruction>(
+                        method.Body.Instructions
+                    );
+            }
+        }
+
+        private ObservableCollection<PapyrusInstruction> selectedMethodInstructions;
+        private ObservableCollection<PapyrusViewModel> pexTree;
+
+        public ObservableCollection<PapyrusInstruction> SelectedMethodInstructions
+        {
+            get { return selectedMethodInstructions; }
+            set { Set(ref selectedMethodInstructions, value); }
+        }
+
+        public ICommand OpenPexCommand { get; set; }
+
+        public RelayCommand SavePexCommand { get; set; }
+
+        public RelayCommand SavePexAsCommand { get; set; }
+
+        public ICommand ExitCommand { get; set; }
+
+
+        public RelayCommand<PapyrusInstruction> InsertBeforeCommand { get; set; }
+
+        public RelayCommand<PapyrusInstruction> InsertAfterCommand { get; set; }
+
+        public RelayCommand<PapyrusInstruction> EditInstructionCommand { get; set; }
+
+        public RelayCommand<PapyrusInstruction> RemoveInstructionCommand { get; set; }
+
+        public RelayCommand<PapyrusViewModel> SelectedMemberCommand { get; set; }
+
+
+        private static MainWindowViewModel designInstance;
+
+        public static MainWindowViewModel DesignInstance = designInstance ??
+                                                           (designInstance = new MainWindowViewModel());
+
+    }
+}
---
+++ Source/PapyrusDotNet.PexInspector.ViewModels/PapyrusViewModel.cs
@@ -0,0 +1,123 @@
+﻿using System.Collections.ObjectModel;
+using System.Windows.Media;
+using GalaSoft.MvvmLight;
+using PapyrusDotNet.PapyrusAssembly;
+
+namespace PapyrusDotNet.PexInspector.ViewModels
+{
+    public class PapyrusViewModel : ViewModelBase
+    {
+        private bool isSelected;
+        private bool isExpanded;
+        private string icon;
+        private string text;
+        private object item;
+
+        public object Item
+        {
+            get { return item; }
+            set
+            {
+                if (Set(ref item, value))
+                {
+                    SetIconFromItem(value);
+                }
+            }
+        }
+
+        private void SetIconFromItem(object value)
+        {
+
+            string packUri = "pack://application:,,,/PapyrusDotNet.PexInspector;component/";
+
+            if (value == null || value.ToString() == "root")
+            {
+                Icon = packUri + "Assets/Icons/type.png";
+            }
+            else if (value != null && value.ToString() == "states")
+            {
+                Icon = packUri + "Assets/Icons/states.png";
+            }
+            else
+            if (value is PapyrusStateDefinition)
+            {
+                Icon = packUri + "Assets/Icons/state.png";
+            }
+            else
+            if (value is PapyrusPropertyDefinition)
+            {
+                Icon = packUri + "Assets/Icons/property.png";
+            }
+            else if (value is PapyrusFieldDefinition)
+            {
+                Icon = packUri + "Assets/Icons/field.png";
+            }
+            else if (value is PapyrusTypeDefinition)
+            {
+                var type = value as PapyrusTypeDefinition;
+                if (type.IsStruct)
+                {
+                    Icon = packUri + "Assets/Icons/structure.png";
+                }
+                else
+                {
+                    Icon = packUri + "Assets/Icons/typedefinition.png";
+                }
+            }
+            else if (value is PapyrusMethodDefinition)
+            {
+                var method = (value as PapyrusMethodDefinition);
+                if (method.IsEvent)
+                    Icon = packUri + "Assets/Icons/event.png";
+                else
+                {
+                    Icon = packUri + "Assets/Icons/method.png";
+                }
+            }
+        }
+
+        public struct ss
+        {
+
+        }
+
+        public PapyrusViewModel Parent { get; set; }
+
+        public ObservableCollection<PapyrusViewModel> Children { get; set; }
+
+        public PapyrusViewModel(PapyrusViewModel parent = null)
+        {
+            if (parent != null)
+            {
+                parent.Children.Add(this);
+            }
+
+            Children = new ObservableCollection<PapyrusViewModel>();
+
+        }
+
+        public string Text
+        {
+            get { return text; }
+            set { Set(ref text, value); }
+        }
+
+        public string Icon
+        {
+            get { return icon; }
+            set { Set(ref icon, value); }
+        }
+
+        public bool IsSelected
+        {
+            get { return isSelected; }
+            set { Set(ref isSelected, value); }
+        }
+
+        public bool IsExpanded
+        {
+            get { return isExpanded; }
+            set { Set(ref isExpanded, value); }
+        }
+    }
+}
